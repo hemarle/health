b0a8c09ff130b77b72bdc2dd6b546ec5a73e1825
William Ndubuisi <princewill835@gmail.com>
2023-11-26 18:24:26 +0100

    dashboard done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Box } from "@mui/material";
-import Layout from "./components/layout/Layout";
-import Chat from "./pages/chat/Chat";
-import Landingpage from "./pages/landingpage/Landingpage";
-import { Routes, Route } from "react-router-dom"
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@mui/material';
+import Layout from './components/layout/Layout';
+import Chat from './pages/chat/Chat';
+import Landingpage from './pages/landingpage/Landingpage';
+import Dashboard  from './pages/dashboard/Dashboard';
+import { Routes, Route } from 'react-router-dom';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#00B277",
-        contrastText: '#fff'
-      }
-    }
-  })
-  return (
-    <ThemeProvider theme={theme}>
-
-    <Routes>
-      <Route path="/" element={<Landingpage />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
-    </ThemeProvider>
-   
-  );
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: '#00B277',
+				contrastText: '#fff',
+			},
+		},
+	});
+	return (
+		<ThemeProvider theme={theme}>
+			<Routes>
+				<Route path="/" element={<Landingpage />} />
+				<Route path="/chat" element={<Chat />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</ThemeProvider>
+	);
 }
 
 export default App;
